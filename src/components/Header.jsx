251f49e51c778aed2064be5c34cd47fb9e4b52c6
andykr1k <69021102+andykr1k@users.noreply.github.com>
2022-07-13 15:07:32 -0700

    Design Complete/Needs Routing

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,10 @@
 const Header = () => {
 
     return (
-      <div className="mt-5">
-        <div className="text-slate-400 text-xl"> Hello, I'm</div>
+      <div className="mt-5 tracking-widest">
+        <div className="text-gray text-xl"> Hello, I'm</div>
         <div className="text-white text-3xl m-2"> Andrew Krikorian </div>
-        <div className="text-slate-400 text-xl"> Developer - Designer - Engineer</div>
+        <div className="text-gray text-xl"> Developer - Designer - Engineer</div>
       </div>
     )
   }
