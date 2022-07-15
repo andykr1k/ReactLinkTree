bc9d3d0e1a3d20321d8d06279e69f8b32b43d0f4
andykr1k <69021102+andykr1k@users.noreply.github.com>
2022-07-15 15:21:27 -0700

    Updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Header, Links, ProfilePic, Footer, SecondLinks} from './components';
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route, Link, BrowserRouter } from "react-router-dom";
 import Projects from "./Projects"
 
 const App = () => {
@@ -7,14 +7,14 @@ const App = () => {
   return (
     <div className='items-center text-center justify-center object-center'>
       <Header />
-      <ProfilePic />
-      <Routes>
-        <Route path='/' element={<Links />} />
-        <Route path='/Projects' element={<SecondLinks />} />
-      </Routes>
-      <div className='m-10'>
-        <Footer />
-      </div>
+        <ProfilePic />
+        <Routes>
+          <Route path='/' element={<Links />} />
+          <Route path='/Projects' element={<SecondLinks />} />
+        </Routes>
+        <div className='m-5'>
+          <Footer />
+        </div>
     </div>
   )
 }
