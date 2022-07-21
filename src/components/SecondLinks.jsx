70f7958a851a5897c80162dbd79ea65cce9114a5
andykr1k <69021102+andykr1k@users.noreply.github.com>
2022-07-21 14:33:46 -0700

    Adding New Project Link

--- src/components/SecondLinks.jsx
+++ src/components/SecondLinks.jsx
@@ -1,5 +1,4 @@
 const SecondLinks = () => {
-
     return (
     <div className="text-white grid place-items-center">
         <div className="border-2 m-2 p-5 w-1/2 lg:w-3/12 rounded-lg lg:hover:bg-white lg:hover:text-black">
@@ -7,6 +6,9 @@ const SecondLinks = () => {
         </div>
         <div className="border-2 m-2 p-5 w-1/2 lg:w-3/12 rounded-lg lg:hover:bg-white lg:hover:text-black">
             <a href="https://starlit-baklava-3e7c38.netlify.app/">CS Club Website</a>
+        </div>
+        <div className="border-2 m-2 p-5 w-1/2 lg:w-3/12 rounded-lg lg:hover:bg-white lg:hover:text-black">
+            <a href="    https://cryptowebtracker.netlify.app/">Crypto Website</a>
         </div>
         <div className="border-2 m-2 p-5 w-1/2 lg:w-3/12 rounded-lg lg:hover:bg-white lg:hover:text-black">
             <a href="/">Home</a>
