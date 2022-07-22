9d7c1ac4d8435ad2006711164aae872d71d4d9f6
andykr1k <69021102+andykr1k@users.noreply.github.com>
2022-07-22 16:49:27 -0700

    Adding New Link

--- src/components/SecondLinks.jsx
+++ src/components/SecondLinks.jsx
@@ -2,13 +2,16 @@ const SecondLinks = () => {
     return (
     <div className="text-white grid place-items-center">
         <div className="border-2 m-2 p-5 w-1/2 lg:w-3/12 rounded-lg lg:hover:bg-white lg:hover:text-black">
+            <a href="https://edvisor.netlify.app/">Edvisor</a>
+        </div>
+        <div className="border-2 m-2 p-5 w-1/2 lg:w-3/12 rounded-lg lg:hover:bg-white lg:hover:text-black">
             <a href="https://jadaganim.com">Portfolio Website</a>
         </div>
         <div className="border-2 m-2 p-5 w-1/2 lg:w-3/12 rounded-lg lg:hover:bg-white lg:hover:text-black">
-            <a href="https://starlit-baklava-3e7c38.netlify.app/">CS Club Website</a>
+            <a href="https://starlit-baklava-3e7c38.netlify.app/">CS Club</a>
         </div>
         <div className="border-2 m-2 p-5 w-1/2 lg:w-3/12 rounded-lg lg:hover:bg-white lg:hover:text-black">
-            <a href="https://cryptowebtracker.netlify.app/">Crypto Website</a>
+            <a href="https://cryptowebtracker.netlify.app/">Crypto Tracker</a>
         </div>
         <div className="border-2 m-2 p-5 w-1/2 lg:w-3/12 rounded-lg lg:hover:bg-white lg:hover:text-black">
             <a href="/">Home</a>
