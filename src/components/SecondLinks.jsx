f266fdfa4bede0248f7d3871c7e867d043b72bc3
andykr1k <69021102+andykr1k@users.noreply.github.com>
2022-08-08 14:15:54 -0700

    fixing UI issues

--- src/components/SecondLinks.jsx
+++ src/components/SecondLinks.jsx
@@ -11,9 +11,6 @@ const SecondLinks = () => {
             <a href="https://starlit-baklava-3e7c38.netlify.app/">CS Club</a>
         </div>
         <div className="border-2 m-2 p-5 w-1/2 lg:w-3/12 rounded-lg lg:hover:bg-white lg:hover:text-black">
-            <a href="https://cryptowebtracker.netlify.app/">Crypto Tracker</a>
-        </div>
-        <div className="border-2 m-2 p-5 w-1/2 lg:w-3/12 rounded-lg lg:hover:bg-white lg:hover:text-black">
             <a href="/">Home</a>
         </div>
       </div>
